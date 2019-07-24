88865f4ec68598ac1cb169c93e79775fbbe7aa7b
Drew Barnett <drew.barnett@createthenext.com>
2019-07-24 10:12:56 -0700

    Added frame stacking functionality to space_invaders script.

--- space_invaders_deepq.py
+++ space_invaders_deepq.py
@@ -20,15 +20,45 @@ def processFrame(frame):
 	normalized = cropped / 255.0
 	return transform.resize(normalized, [110, 84])
 
+def stackFrames(stacked_frames, new_frame, new_episode):
+	return_stack = stacked_frames
+	return_state = None
+	frame = processFrame(new_frame)
+
+	if new_episode:
+		for _ in range(stack_size):
+			return_stack.append(frame)
+	else:
+		return_stack.append(frame)
+
+	return_state = np.stack(return_stack, axis=2)
+	return return_state, return_stack
+
 env = gym.make("SpaceInvaders-v0")
 state_space = env.observation_space
 action_space = env.action_space.n
 action_code = np.identity(action_space, dtype=int).tolist()
 
+stack_size = 4
+blank_imgs = [np.zeros((110, 84), dtype=np.int) \
+					   for i in range(stack_size)]
+frame_stack = deque(blank_imgs, maxlen = stack_size)
+
 '''
+# testing
 env.reset()
 obs, _, _, _ = env.step(env.action_space.sample())
-result = processFrame(obs)
-print(result)
-print(type(result))
+frame = processFrame(obs)
+result1, result2 = stackFrames(frame_stack, frame, True)
+print(result1)
+print(type(result1))
+print(result2)
+print(type(result2))
+obs, _, _, _ = env.step(env.action_space.sample())
+frame = processFrame(obs)
+result1, result2 = stackFrames(frame_stack, frame, False)
+print(result1)
+print(type(result1))
+print(result2)
+print(type(result2))
 '''
